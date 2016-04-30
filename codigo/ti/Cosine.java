b384688d96411463d4e7ad5b6538cd67351b39e4
GabiArroyo <gabriel.arroyo01@estudiant.upf.edu>
2016-05-01 01:47:16 +0200

    Cosine: Añadido método de Jaccard y Dice para cálculo de las similudes. Retocada función compute vector, error en la comparación de strings, el operador == compara instancias no strings. Comprobado con los resultados de teoría.

--- codigo/ti/Cosine.java
+++ codigo/ti/Cosine.java
@@ -39,22 +39,21 @@ public class Cosine implements RetrievalModel
 	protected ArrayList<Tuple<Integer, Double>> computeScores(ArrayList<Tuple<Integer, Double>> queryVector, Index index)
 	{
 		ArrayList<Tuple<Integer, Double>> results = new ArrayList<>();
-
-		// P1
 		HashMap<Integer,Double> res  = new HashMap<Integer,Double>();
+                
 		double normWtq = 0.0;
-		for(Tuple<Integer,Double> qw : queryVector){
+		for(Tuple<Integer,Double> qw : queryVector)
 			normWtq += qw.item2 * qw.item2;
-		}
                 normWtq = Math.sqrt(normWtq);
                 
 		for(Tuple<Integer,Double> qw : queryVector){//for each query word
 			for(Tuple<Integer, Double> docsW : index.invertedIndex.get(qw.item1)) {
 				if(res.containsKey(docsW.item1) == false)
-                                    res.put(docsW.item1,0.0);
-                                res.replace(docsW.item1,  res.get(docsW.item1) + ((docsW.item2) * (qw.item2))/((index.documents.get(docsW.item1).item2 )*( normWtq )) );
-			}
+                                    res.put(docsW.item1, 0.0);
+                               res.replace(docsW.item1,  res.get(docsW.item1) + (((docsW.item2) * (qw.item2))/((index.documents.get(docsW.item1).item2)*(normWtq))));
+                        }
 		}
+                
 		for(Map.Entry<Integer,Double> e : res.entrySet() ){
 			results.add(new Tuple<Integer, Double>(e.getKey(),e.getValue()));
 		}
@@ -70,6 +69,24 @@ public class Cosine implements RetrievalModel
 		});
 		return results;
 	}
+        
+        // Método Dice
+        // En teoría o en el enlace inferior.
+        // https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient Mitad de la página. (Valor absoluto no es necesario en nuestro caso)
+        void DiceScore(HashMap<Integer,Double> res, Tuple<Integer,Double> qw, Tuple<Integer, Double> docsW, Index index, double normWtq){
+            res.replace(docsW.item1,  res.get(docsW.item1) + (2 * (docsW.item2) * (qw.item2))
+                    /((index.documents.get(docsW.item1).item2 )*( normWtq )) );
+        }
+        
+        // Método de Jaccard
+        // En teoría o en enlace inferior.
+        // https://en.wikipedia.org/wiki/Jaccard_index  Al final de la página.
+        void JaccardScore(HashMap<Integer,Double> res, Tuple<Integer,Double> qw, Tuple<Integer, Double> docsW, Index index, double normWtq){
+            res.replace(docsW.item1,  res.get(docsW.item1) + ((docsW.item2) * (qw.item2))
+                    /((index.documents.get(docsW.item1).item2 )*( normWtq )) - (docsW.item2) * (qw.item2) );
+        }
+        
+        
 
 	/**
 	 * Compute the vector of weights for the specified list of terms.
@@ -87,26 +104,108 @@ public class Cosine implements RetrievalModel
                 //tftq : indicador frecuancia aparicion del termino , suavizando en el numero de apariciones
                 //idft : indicador que penaliza la aparicion de una palabra en una gran cantidad de documentos
                 //wtq : peso de una palabra en la query
-		// P1
-		for(int i = 0 ; i < terms.size() ; ++i){
-			Tuple<Integer, Double> termInfo = index.vocabulary.get(terms.get(i));
-			Double idft = termInfo.item2;
-			int id_term = termInfo.item1;
-			int ftq = 0;
-			boolean isRepited = false;					
-			for(int j = 0 ; j < terms.size() ; ++j){
-				if(terms.get(i) == terms.get(j)){
-					ftq++;
-					if(j < i){
-						isRepited = true;
-						break;
-					}
-				}
-			}
-			if(isRepited)continue;			//Si ya habiamos guardado la informacion de este termino no lo guardamos en el vector de resultados
-			Double tftq = 1 + Math.log(ftq);
-			vector.add(new Tuple<Integer, Double>(id_term,idft*tftq));
+                
+                Double idft, tftq;
+                int id_term, ftq;
+        	for(int i = 0 ; i < terms.size() ; ++i){
+                    Tuple<Integer, Double> termInfo = index.vocabulary.get(terms.get(i));
+                    id_term = termInfo.item1; 
+                    idft = termInfo.item2;
+                    ftq = 0;
+                    boolean isRepited = false;					
+                    for(int j = 0 ; j < terms.size() ; ++j){
+                        if(terms.get(i).compareTo(terms.get(j)) == 0){
+                            ftq++;
+                            if(j < i){
+                                isRepited = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(isRepited)continue;			//Si ya habiamos guardado la informacion de este termino no lo guardamos en el vector de resultados
+                    tftq = 1 + Math.log(ftq);
+                    vector.add(new Tuple<Integer, Double>(id_term,idft*tftq));
 		}
+                
 		return vector;
 	}
+        
+        // Método Okami
+        //B y K son los pesos, estaría mejor dedicirlos y declararlos como constante. Para eso hará falta realizar evaluaciones.
+        protected ArrayList<Tuple<Integer, Double>> computeScoresOkamiBM25(ArrayList<Tuple<Integer, Tuple<Double, String>>> queryVector, 
+                Index index, int k, int b){
+            
+            ArrayList<Tuple<Integer, Double>> results = new ArrayList<>();
+            // P1
+            
+            HashMap<Integer, Double> res  = new HashMap<Integer, Double>();
+            double normWtq = 0.0;
+            for(Tuple<Integer,Tuple<Double, String>> qw : queryVector)
+                normWtq += qw.item2.item1 * qw.item2.item1;
+            normWtq = Math.sqrt(normWtq);
+            
+            for(Tuple<Integer, Tuple<Double, String>> qw : queryVector){//for each query word
+                for(Tuple<Integer, Double> docsW : index.invertedIndex.get(qw.item1)) {
+                    if(res.containsKey(docsW.item1) == false)
+                        res.put(docsW.item1, 0.0);
+                                
+                                int ct =  index.invertedIndex.get(qw.item1).size();
+                                double idf = Math.log((index.documents.size() - ct + 0.5)/(ct + 0.5));
+                                
+                                // docsW.item2 es Wtd y index.vocab es idft, así se obtiene tftd
+                                double tftd = docsW.item2/index.vocabulary.get(qw.item2.item2).item2;
+                                
+                                
+                                
+                                //Falta obtener la longitud del documento y la media de todos ellos.
+                                // Para eso se podría escribir al final del load del índice un bucle que recorra
+                                // todo invertedIndex. Documentos pasaria a ser Tuple<String, Tuple<Double, Integer>>.
+                                //donde int seria el numero de palabras y double la norma.
+                                // Habria que usar los pesos y dividirlos entre idft del termino para encontrar tftd y a partir de ahi ftd
+                                
+                                //double termSup = (k+1) * tftd / (tftd + k * (1 - b + b * (docsW)));
+                              
+                        }
+		}
+		for(Map.Entry<Integer,Double> e : res.entrySet() ){
+			results.add(new Tuple<Integer, Double>(e.getKey(),e.getValue()));
+		}
+		
+		// Ordenar documentos por similitud y devolver
+		Collections.sort(results, new Comparator<Tuple<Integer, Double>>()
+		{
+			@Override
+			public int compare(Tuple<Integer, Double> o1, Tuple<Integer, Double> o2)
+			{
+				return o2.item2.compareTo(o1.item2);
+			}
+		});
+		return results;
+        }
+        
+        
+        // Los valores de este vector son : Integer: TermId. Tupla: -Double: Peso termino query String: Termino
+        protected  ArrayList<Tuple<Integer, Tuple<Double, String>>> computeVectorOkami(ArrayList<String> terms, Index index){
+            ArrayList<Tuple<Integer, Tuple<Double, String>>> vector = new ArrayList<>();
+            
+            for(int i = 0 ; i < terms.size() ; ++i){
+                Tuple<Integer, Double> termInfo = index.vocabulary.get(terms.get(i));
+                Double idft = termInfo.item2;   int id_term = termInfo.item1;
+                int ftq = 0;    boolean isRepited = false;					
+                
+                for(int j = 0 ; j < terms.size() ; ++j){
+                    if(terms.get(i) == terms.get(j)){
+                        ftq++;
+                        if(j < i){
+                            isRepited = true;
+                            break;
+                        }
+                    }
+                }
+                if(isRepited)continue;	//Si ya habiamos guardado la informacion de este término no lo guardamos en el vector de resultados
+                Double tftq = 1 + Math.log(ftq);
+                vector.add(new Tuple<Integer, Tuple<Double, String>>(id_term, new Tuple(idft*tftq, terms.get(i))));
+            }
+            return vector;
+        }
 }
